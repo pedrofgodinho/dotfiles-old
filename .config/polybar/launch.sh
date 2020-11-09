7ea124caa7ec5be4d05d265060c809e7ec7ab11c
Pedro Godinho <pedromfgodinho@gmail.com>
2020-11-09 19:32:12 +0000

    Changed polybar to work with multimonitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,4 +9,11 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  done
+else
+  polybar -c ~/.config/polybar/config.ini main &
+fi
+#polybar -c ~/.config/polybar/config.ini main &
